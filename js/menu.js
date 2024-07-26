1e4d819ad21f9ac3509693524f87526d0fdd62f9
Andrij P. <andrejpopkov565@gmail.com>
2024-07-26 13:27:08 +0300

    latest improvements

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-menu-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-modal-close]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-modal]"),
+    modal: document.querySelector("[data-menu]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
